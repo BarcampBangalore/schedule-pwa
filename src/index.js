6e1506dbec31956b64d35dc8020eb185f262f4ac
tallpants <adithyakreddy6@gmail.com>
2018-09-02 00:22:35 +0530

    Listen for beforeinstallprompt event

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
+
+window.addEventListener('beforeinstallprompt', e => {
+  e.prompt()
+})
